bd4d503943ff21776eaf3ab4bc248e66f9f1e807
Adam Dangoor <adamdangoor@gmail.com>
2019-12-08 00:35:59 +0000

    Initial test for time

--- tests/test_vws_auth_tools.py
+++ tests/test_vws_auth_tools.py
@@ -2,11 +2,36 @@
 XXX
 """
 
+import datetime
+
+import pytz
 from freezegun import freeze_time
 
+import vws_auth_tools
+
 def test_rfc_1123_date():
-    pass
+    """
+    XXX
+    """
+    not_gmt_timezone = pytz.timezone('America/New_York')
+    frozen_time = datetime.datetime(
+        year=2015,
+        month=2,
+        day=5,
+        hour=9,
+        minute=55,
+        second=12,
+        microsecond=11,
+        tzinfo=not_gmt_timezone,
+    )
+    with freeze_time(frozen_time):
+        result = vws_auth_tools.rfc_1123_date()
+
+    assert result == 'Thu, 05 Feb 2015 14:51:12 GMT'
 
 def test_authorization_header():
+    """
+    XXX
+    """
     pass
 
