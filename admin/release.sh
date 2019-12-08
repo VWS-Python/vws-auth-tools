3b0ca525a59c7f4f4502347a667dba73e8de3115
Adam Dangoor <adamdangoor@gmail.com>
2019-12-08 14:43:25 +0000

    Fix release script

--- admin/release.sh
+++ admin/release.sh
@@ -6,7 +6,7 @@ set -ex
 # See the release process documentation for details.
 cd "$(mktemp -d)"
 git clone git@github.com:"${GITHUB_OWNER}"/vws-auth-tools.git
-cd vws-python
+cd vws-auth-tools
 virtualenv -p python3 release
 source release/bin/activate
 pip install --editable .[dev]
