c9a0f4f7febba87ea45482985003e3645b2f7157
Adam Dangoor <adamdangoor@gmail.com>
2024-08-29 12:35:35 +0100

    Switch from typeguard to beartype

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@
 
 from doctest import ELLIPSIS
 
+import pytest
+from beartype import beartype
 from sybil import Sybil
 from sybil.parsers.rest import (
     CaptureParser,
@@ -19,3 +21,12 @@ sybil_obj = Sybil(
 )
 
 pytest_collect_file = sybil_obj.pytest()
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
+    """
+    Apply the beartype decorator to all collected test functions.
+    """
+    for item in items:
+        if isinstance(item, pytest.Function):
+            item.obj = beartype(obj=item.obj)
